cf1eac4df499eacab294d87a17921ef5d517559d
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2021-10-31 19:15:20 +0530

    Update school_bell.ino

--- school_bell.ino
+++ school_bell.ino
@@ -1,206 +1,333 @@
-#include <Wire.h>
+#include <MFRC522.h>
+#include <SPI.h>
+#include <SD.h>
 #include <RTC.h>
+#include <LiquidCrystal.h>
 
-DS3231 rtc;					// the DS3231 RTC
-const int buzzer = 9;		// Buzzer Pin or Relay Pin
-const int pushButton = 7;	// The push button to manually ring the bell
-int pushButtonVal = LOW;
+// define Pins for RFID
+#define CS_RFID 10
+#define RST_RFID 9
 
-int currentHours = 0;		// Stores the current hour
-int currentMinutes = 0;		// Stores the current minute
+// define select pins for SD
+#define CS_SD 8
 
-//char daysOfTheWeek[7] = {"Sunday", "Monday", "Tuesday", "Wednesday", 
-//						"Thursday", "Friday", "Saturday"};
+File file;
+MFRC522 rfid(CS_RFID, RST_RFID);
+MFRC522::MIFARE_Key key;
+DS3231 rtc;
 
-// The Array of bell ringing Hours
-int bellTimingsHours[6] = {6, 7, 8, 9, 10, 11};
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-// The Array of bell ringing Minutes
-int bellTimingsMinutes[4] = {15, 40, 45, 55};
+// const int numStudents = 3;
+#define numStudents 3
 
-// Constant length for assurance of correct mapping
-const int bellTimingsLen = 12;
-
-// Array of bell ring types => (cotinued long or n times)
-int bellTypes[bellTimingsLen] = {
-	// 0 = long bell
-	0, 1, 2, 3, 4, 0, 0, 5, 6, 7, 8, 0
-	// depart for assembly, 1, 2, 3, 4 bells
-	// lunch begin bell, lunch end bell (and the 5th period bell)
-	// 6, 7, 8 bells, then finally, Final Bell!!!
+const long UID_ARRAY[numStudents] = {
+    171819116000143, 171819116000144, 171819116000145,
 };
 
-// Array of the duration of bell rings in millis
-int bellLongDurations[bellTimingsLen] = {
-	3000, 1000, 1000, 1000, 1000, 4000, 4000, 1000, 1000, 1000, 1000, 5000
-};
+// const int UID_BLOCK = 1;
+// const int NAME_BLOCK = 2;
+#define UID_BLOCK 1
+#define NAME_BLOCK 2
 
-/*
-// Old Stuff, don't notice
-typedef struct hourMinute {
-	int hour;
-	int minute;
-} hourMinute_t;
+long currentUID;
+char currentName[18];
 
-hourMinute_t bellTimings[] = {
-	{6, 55}, {7, 15}, {7, 45}, {8, 15}, {8, 45}, 
-	{9, 15}, {9, 45}, {10, 15}, {10, 45},
-	{11, 15}, {11, 45}
-};
-*/
+// Latest Attendence Time
+// const int checkInHour = 6;
+// const int checkInMinute = 55;
+#define checkInHour 6
+#define checkInMinute 55
 
-// 2-dimensional array of the bell timings, in the form of {hour, minute}
-// Note that every hour and minute present here is in the array of (bellTimingsHours) and (bellTimingsMinutes), respectively
-// Also the length of this array and the arrays of durations and types is same (bellTimingsLen)
-// This is done ensure that the mapping between the arrays is correct
-int bellTimings[bellTimingsLen][2] = {
-	{6, 55}, {7, 15}, {7, 45}, {8, 15}, {8, 45}, 
-	{9, 15}, {9, 40}, {9, 45}, {10, 15}, {10, 45},
-	{11, 15}, {11, 45},
-};
+// Current Student's Arrival Time
+byte studentCheckInHour;
+byte studentCheckInMinute;
 
-// This block will run only one time or every time reset button is pressed, when the arduino is powered
+// LED's Pins
+#define RED_LED 6
+#define GREEN_LED 7
+
 void setup() {
-	Serial.begin(9600);
-	
-	if (!rtc.begin()) {
-		Serial.println("Couldn't Find RTC");
-		while(1);
-		
-	} else {
-		// Set the date and time when the sketch was compiled
-		// For the first time only, comment afterwards
-		rtc.setDateTime(__DATE__, __TIME__);
-		
-		// Or setting it absolutely
-		// rtc.setDate(2021, 9, 23);
-		// rtc.setTime(4, 31, 30);
+    // Set LEDs to OUTPUT
+    pinMode(RED_LED, OUTPUT);
+    pinMode(GREEN_LED, OUTPUT);
+    
+    Serial.begin(9600);
+    
+    SPI.begin();
+    rfid.PCD_Init();
+    
+    Serial.println(F("Initializing SD Card"));
+    
+    if (!SD.begin(CS_SD)) {
+        Serial.println(F("SD Card Initialization Failed"));
+        while(1);
+    }
+    
+    Serial.println(F("SD Card Initialization Done"));
+    
+    if (!rtc.begin()) {
+        Serial.println(F("Couldn't Find RTC"));
+        while(1);
+        
+    } else {
+        // Set the date and time when the sketch was compiled
+        // For the first time only, comment afterwards
+        rtc.setDateTime(__DATE__, __TIME__);
+        
+        // Or setting it absolutely
+        // rtc.setDate(2021, 9, 23);
+        // rtc.setTime(4, 31, 30);
+        
+        // Comment out this also after setting date time...
+        Serial.println(F("Date Time set!"));
+    }
+    
+    if (!rtc.isRunning()) {
+        Serial.println(F("RTC Not Running"));
+    }
 
-		Serial.println("Date Time set!");
-	}
-	
-	if (!rtc.isRunning()) {
-		Serial.println("RTC Not Running!");
-	}
+    lcd.begin(16, 2);
+    lcd.print(F("Hello School!"));
+    delay(1500);
+    lcd.clear();
 
-	pinMode(buzzer, OUTPUT);	// Set the buzzer pin to OUTPUT
-	pinMode(pushButton, INPUT);	// Set the pushButton pin to INPUT
+    // Prepare key - all keys are set to FFFFFFFFFFFFh at chip delivery from the factory.
+    for (byte i = 0; i < 6; i++) key.keyByte[i] = 0xFF;
 }
 
-// This block will run until you die or the power goes off...
+// Used for finding the index of an long in an array
+int indexOfLong(const long ar[], int len, long elem) {
+    for (int i = 0; i < len; ++i)
+    {
+        if (elem == ar[i]) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+// Reads the RFID card
+void readRFID() {
+        
+    // Dump UID
+    Serial.print(F("Card UID:"));
+    for (byte i = 0; i < rfid.uid.size; i++) {
+        Serial.print(rfid.uid.uidByte[i] < 0x10 ? " 0" : " ");
+        Serial.print(rfid.uid.uidByte[i], HEX);
+    }
+    
+    // We want to somehow store and get
+    // the student UID and student Name from the Card
+    // Guess Done...
+
+    MFRC522::StatusCode status;
+
+    lcd.clear();
+    lcd.setCursor(0, 0);
+
+    // Authenticate for UID_BLOCK
+    status = rfid.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, UID_BLOCK, &key, &(rfid.uid));
+    if (status != MFRC522::STATUS_OK) {
+        Serial.print(F("Authentication failed: "));
+        Serial.println(rfid.GetStatusCodeName(status));
+        lcd.print(F("Authentication Failed: Contact Us"));
+        return;
+    }
+
+    byte temp_read[18];
+    // Read UID from the card
+    status = rfid.MIFARE_Read(UID_BLOCK, temp_read, 18);
+    if (status != MFRC522::STATUS_OK) {
+        Serial.print(F("Reading UID failed: "));
+        Serial.println(rfid.GetStatusCodeName(status));
+        lcd.print(F("Reading from Card Failed: Contact Us"));
+        return;
+    }
+
+    currentUID = atol(temp_read);
+
+    // Authenticate for NAME_BLOCK
+    status = rfid.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, NAME_BLOCK, &key, &(rfid.uid));
+    if (status != MFRC522::STATUS_OK) {
+        Serial.print(F("Authentication failed: "));
+        Serial.println(rfid.GetStatusCodeName(status));
+        lcd.print(F("Authentication Failed: Contact Us"));
+        return;
+    }
+
+    // Read Name from the card
+    status = rfid.MIFARE_Read(NAME_BLOCK, temp_read, 18);
+    if (status != MFRC522::STATUS_OK) {
+        Serial.print(F("Reading Name failed: "));
+        Serial.println(rfid.GetStatusCodeName(status));
+        lcd.print(F("Reading from Card Failed: Contact Us"));
+        return;
+    }
+
+    // currentName = atol(temp_read);
+    *currentName = reinterpret_cast<const char*>(temp_read);
+    
+    int i = indexOfLong(UID_ARRAY, numStudents, currentUID);
+    if (i != -1) {
+        //currentName = NAMES[i];
+        digitalWrite(GREEN_LED, HIGH);
+        // Print Name
+        Serial.print(F("Welcome "));
+        Serial.println(currentName);
+
+        lcd.print(F("Welcome "));
+        lcd.print(currentName);
+
+        delay(1500);
+        digitalWrite(GREEN_LED, LOW);
+
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        
+        // Print UID
+        Serial.print(F("Your UID is: "));
+        Serial.println(currentUID);
+        
+        Serial.println(F("\n**End Reading**\n"));
+        delay(1000); //change value if you want to read cards faster
+        rfid.PICC_HaltA();
+        rfid.PCD_StopCrypto1();
+        
+        markAttendance();
+    } else {
+        digitalWrite(RED_LED, HIGH);
+        Serial.println(F("Sorry, We Don't know you!"));
+        lcd.print(F("Sorry, We Don't know you!"));
+        delay(2000);
+        digitalWrite(RED_LED, LOW);
+    }
+}
+
+// Marks Attendence
+void markAttendance() {
+    Serial.println(F("Marking You Attendance"));
+    lcd.print(F("Marking Your Attendance"));
+    // Enable the SD Card chip select Pin
+    digitalWrite(CS_SD, LOW);
+    
+    studentCheckInHour = rtc.getHours();
+    studentCheckInMinute = rtc.getMinutes();
+    int seconds = rtc.getSeconds();
+
+    lcd.clear();
+    // Print Current Time
+    lcd.print(studentCheckInHour);
+    lcd.print(F(":"));
+    lcd.print(studentCheckInMinute);
+    lcd.print(F(":"));
+    lcd.print(seconds);
+    
+    int date = rtc.getDay();
+    int month = rtc.getMonth();
+    int year = rtc.getYear();
+    
+    lcd.clear();
+    // Print Today's Date
+    lcd.print(date);
+    lcd.print(F("-"));
+    lcd.print(month);
+    lcd.print(F("-"));
+    lcd.print(year);
+
+    char filename = "Attendance_";
+    sprintf(filename, "%d_%d_%d.txt", year, month, date); 
+    file = SD.open(filename, FILE_WRITE);
+    
+    if (file) {
+        Serial.println(F("File Opened Ok"));
+        delay(500);
+        
+        // // Write the UID to file.
+        // for (uint8_t i = 0; i < 16; i++)
+        // {
+        //     if (currentUID[i] != 32) file.write(currentUID[i]);
+        // }
+        file.print(currentUID);
+        file.print(F(","));
+        
+        // Then the Time in HH:MM:SS Format
+        if (studentCheckInHour < 9) {
+            file.print(0);
+            file.print(studentCheckInHour);
+        } else {
+            file.print(studentCheckInHour);
+        }
+        file.print(F(":"));
+        if (studentCheckInMinute < 9) {
+            file.print(0);
+            file.print(studentCheckInMinute);
+        } else {
+            file.print(studentCheckInMinute);
+        }
+        file.print(F(":"));
+        if (seconds < 9) {
+            file.print(0);
+            file.print(seconds);
+        } else {
+            file.print(seconds);
+        }
+        
+        file.print(F(","));
+
+        lcd.clear();
+        // Then whether the student was on time?
+        if ((studentCheckInHour < checkInHour) || (studentCheckInHour == checkInHour) && (studentCheckInMinute <= checkInMinute)) {
+            digitalWrite(GREEN_LED, HIGH);
+            Serial.println(F("You are on Time!"));
+            
+            lcd.print(F("You are on Time"));
+            file.println(F("True"));
+            delay(500);
+            digitalWrite(GREEN_LED, LOW);
+            
+        } else {
+            digitalWrite(RED_LED, HIGH);
+            Serial.println(F("You are Late!"));
+            
+            lcd.print(F("You are Late Today"));
+            file.println(F("False"));
+            delay(500);
+            digitalWrite(RED_LED, LOW);
+        }
+        
+        delay(500);
+        
+        file.close();
+        
+    } else {
+        Serial.println(F("Error opening file, Contact the maintainers of this thing."));
+        lcd.print(F("Error Opening File"));
+    }
+    
+    // Disable the SD Card Chip sleect Pin
+    digitalWrite(CS_SD, HIGH);
+}
+
+
 void loop() {
+    // Look for new cards
+    if (!rfid.PICC_IsNewCardPresent()) 
+        return;
+    // Select one of the cards
+    if (!rfid.PICC_ReadCardSerial()) 
+        return;
 
-	pushButtonVal = digitalRead(pushButton);	// Read the pushButton value as either pressed or not pressed
+    lcd.print(F("Card Found"));
+    lcd.setCursor(0, 1);
+    lcd.print(F("Scanning it..."));
+    
+    readRFID();
 
-	if (pushButtonVal == HIGH) {
-		Serial.println("Push button pressed!");
-		if (digitalRead(buzzer) == LOW)
-			digitalWrite(buzzer, HIGH);		// If pushButton is pressed and the buzzer is off, turn on the buzzer
-	} else {
-
-		if (digitalRead(buzzer) == HIGH)
-			digitalWrite(buzzer, LOW);		// If pushButton is not pressed and buzzer is on, turn off the buzzer
-	}
-	
-	// DateTime now = DateTime(rtc.getYear(), rtc.getMonth(), rtc.getDay(), rtc.getHours(), rtc.getMinutes(), rtc.getSeconds());
-	// Serial.println(now);
-
-	if (rtc.getWeek() == 1) {	// 1 is sunday, varies from 1 (sunday) to 7 (saturday)
-		return;					// Do nothing
-	}
-	
-	currentHours = rtc.getHours();
-	currentMinutes = rtc.getMinutes();
-
-	// Info the time to the serial monitor
-	Serial.print("Time is ");
-	Serial.print(currentHours);
-	Serial.print(":");
-	Serial.print(currentMinutes);
-	Serial.println();
-	
-	// This means that if (currentHour) is in the array of (bellTimingsHours), i.e. index != -1
-	// if index == -1, then the element is not in the array
-	if (indexOfInt(bellTimingsHours, currentHours) != -1) {
-		if (indexOfInt(bellTimingsMinutes, currentMinutes) != -1) {
-			// It is bell time !!!
-			ringBell();
-		}
-	}
-
-	delay(5000);	// Wait for 5 seconds before checking again
+    lcd.clear();
+    lcd.print(F("Welcome to the Class"));
+    
+    delay(10);
 }
-
-// Used for finding the index of an int element in an array
-int indexOfInt(int ar[], int elem) {
-	int index = -1;
-	int len = sizeof(ar) / sizeof(ar[0]);
-	for (int i = 0; i < len; ++i)
-	{
-		if (elem == ar[i]) {
-			index = i;
-			break;
-		}
-	}
-
-	return index;
-}
-
-// Used for finding the index of an array in another 2-dimensional array with the length of inner array = 2
-int indexOfArray(int ar[][2], int elem[]) {
-	int index = -1;
-	int len = sizeof(ar) / sizeof(ar[0]);
-	for (int i = 0; i < len; ++i)
-	{
-		if (elem == ar[i]) {
-			index = i;
-			break;
-		}
-	}
-
-	return index;
-}
-
-// Ring the bell (n) number of times each for (duration) millis with a (pause) millis interval
-void ringBell_ntimes(int n = 3, int duration = 1000, int pause = 500) {
-	//repeat(n)
-		// Start Bell for duration
-		// Stop bell for pause
-
-  for (int i = 0; i < n; ++i)
-  {
-	digitalWrite(buzzer, HIGH);
-	delay(duration); // Wait duration millis
-	digitalWrite(buzzer, LOW);
-	delay(pause); // Wait pause millis
-  }
-}
-
-// Ring the bell continuously for (duration) millis
-void ringBell_long(int duration = 5000) {
-	// Start Bell for duration
-	// Stop bell
-
-	digitalWrite(buzzer, HIGH);
-	delay(duration);
-	digitalWrite(buzzer, LOW);
-}
-
-// Processes the (currentHour) and (currentMinute) to find the type and duration of the bell to be rang
-// Also calls the correct function with the required parameters
-void ringBell() {
-	int hmArray[2] = {currentHours, currentMinutes};	// Construct an array of (currentHour) and (currentMinute)
-	int index = indexOfArray(bellTimings, hmArray);		// get the index of the constructed array in the (bellTimings) array
-	int typeOfBell = bellTypes[index];					// get the type of the bell using that index
-	int duration = bellLongDurations[index];			// get the duration of the bell using that index
-
-	// Call the correct ringing function with the reuqired params based on the type of bell
-	if (typeOfBell == 0) {
-		ringBell_long(duration);
-	} else {
-		ringBell_ntimes(typeOfBell, duration);
-	}
-}
-
-/* Code by Harsh Narayan Jha and team */
-/* Last Updated: 03-10-2021 at 07:01 pm */
