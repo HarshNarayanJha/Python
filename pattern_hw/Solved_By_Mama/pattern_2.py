085a44f177d740fa8dcd62c08fe999c068f66ad2
abhigureniya <abhigureniya1@gmail.com>
2022-02-02 16:31:38 +0530

    Update pattern_2.py

--- pattern_hw/Solved_By_Mama/pattern_2.py
+++ pattern_hw/Solved_By_Mama/pattern_2.py
@@ -33,6 +33,22 @@ while True:
 	
 	print(toPrint)
 	currentLevel = currentLevel + 1
+
+#Source Geeks for Geeks
+#Method 1: Using nCr formula i.e. n!/(n-r)!r!
+#After using nCr formula, the pictorial representation becomes:
+#         0C0
+#      1C0   1C1
+#   2C0   2C1   2C2
+#3C0   3C1   3C2    3C3
+from math import factorial
+levels = 8
+for i in range(levels):
+    print(" "*(levels-(i+1)),end="")
+    for j in range(i+1):
+        print(factorial(i)//(factorial(i-j)*factorial(j)),end=" ")
+    print()
+
 	prevlevelNums = currentlevelNums.copy()
 
 
